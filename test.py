68b23634e786398b3ee15e7efa1fb1b09d63943c
Ross Burton <ross@linux.intel.com>
2009-08-03 09:37:11 +0100

    Add more tests

--- test.py
+++ test.py
@@ -1,21 +1,29 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from tumblrss import tumblrss
+
+def check(a, b):
+    if a == b:
+        return
+
+    print "'%s' != '%s'" % (a, b)
+    raise AssertionError
 
 def compare(items, expected):
     assert len(items) == len(expected)
     for i in range(0, len(items)):
-        assert items[i].author == expected[i][0]
-        assert items[i].link == expected[i][1]
-        assert items[i].guid == expected[i][1]
+        check(items[i].author, expected[i][0])
+        check(items[i].link, expected[i][1])
+        check(items[i].guid, expected[i][1])
         if len(expected[i]) == 3:
-            assert items[i].title == expected[i][2]
+            check(items[i].title, expected[i][2])
         else:
-            assert items[i].title == ""
+            check(items[i].title, "")
 
 
 (items, url) = tumblrss(open("tests/test-1.html").read())
-assert url == "http://tumblr.com/dashboard/2/151787926"
+check (url, "http://tumblr.com/dashboard/2/151787926")
 assert len(items) == 5
 compare(items, (
         ("rulesformyunbornson",
@@ -28,7 +36,7 @@ compare(items, (
          ),
         ("joshual",
          "http://joshual.tumblr.com/post/152893624/xml-is-like-violence-if-it-doesnt-solve-your",
-         "nokogiri-1.3.2 Documentation"
+         u"nokogiri-1.3.2 Documentation | I’m now sold on XML!"
          ),
         ("conundrum",
          "http://conundrum.tumblr.com/post/152196239/dottorcarlo-puscic-comicbooks-death-by-charles"
@@ -39,7 +47,7 @@ compare(items, (
         ))
 
 (items, url) = tumblrss(open("tests/test-2.html").read())
-assert url == "http://tumblr.com/dashboard/3/150772409"
+check(url, "http://tumblr.com/dashboard/3/150772409")
 assert len(items) == 7
 compare(items, (
         ("conundrum",
@@ -57,6 +65,36 @@ compare(items, (
          "http://conundrum.tumblr.com/post/150856620/dan-zambonini-london-tube-map-hat-tip-thomas"),
         ("joshual",
          "http://joshual.tumblr.com/post/150772409/the-game-named-page-hunt-presents-users-with-a",
-         "Microsoft Bing Could Be Improved with Online Game"
+         u"Microsoft Bing Could Be Improved with Online Game\nYeah, “game” …"
          )
         ))
+
+
+(items, url) = tumblrss(open("tests/test-3.html").read())
+check(url, "http://tumblr.com/dashboard/4/150347591")
+assert len(items) == 9
+compare(items, (
+        ("joshual",
+         "http://joshual.tumblr.com/post/150985551/management-bullshit-generator-from-buzzwords4u-co-uk",
+         "Management Bullshit Generator from buzzwords4u.co.uk"),
+        ("glinner",
+         "http://glinner.tumblr.com/post/150884779/dan-haggerty-kickin-bbq-sauce"),
+        ("joshual",
+         "http://joshual.tumblr.com/post/150860306/awesome-dan-zambonini"),
+        ("conundrum",
+         "http://conundrum.tumblr.com/post/150856620/dan-zambonini-london-tube-map-hat-tip-thomas"),
+        ("joshual",
+         "http://joshual.tumblr.com/post/150772409/the-game-named-page-hunt-presents-users-with-a",
+         u"Microsoft Bing Could Be Improved with Online Game\nYeah, “game” …"
+         ),
+        ("conundrum",
+         "http://conundrum.tumblr.com/post/150742879/we-welcome-people-of-any-gender-identity-or",
+         u"Dreamwidth’s Diversity Statement | I want this on every open source  project I partecipate or contribute to."),
+        ("rulesformyunbornson",
+         "http://rulesformyunbornson.tumblr.com/post/150392453/mac-makes-a-strong-case-for-pleats"),
+        ("rulesformyunbornson",
+         "http://rulesformyunbornson.tumblr.com/post/150391892/never-give-an-order-that-cant-be-obeyed",
+         "Gen. Douglas MacArthur"),
+        ("rulesformyunbornson",
+         "http://rulesformyunbornson.tumblr.com/post/150347591/required-listening-crowded-house-something-so")
+))
